fb0f947967a04fea0cf04a1d2761f4131e75d568
TuCodigoProgramacion <rolando.perez@uteco.edu.do>
2021-07-15 20:14:28 -0400

    Primer Commit

---
+++ pjtImagenbaseDatos/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace pjtImagenbaseDatos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ptbImagen = new System.Windows.Forms.PictureBox();
+            this.dgvImagen = new System.Windows.Forms.DataGridView();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnExaminar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbImagen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvImagen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ptbImagen
+            // 
+            this.ptbImagen.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ptbImagen.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.ptbImagen.Location = new System.Drawing.Point(479, 46);
+            this.ptbImagen.Name = "ptbImagen";
+            this.ptbImagen.Size = new System.Drawing.Size(163, 134);
+            this.ptbImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbImagen.TabIndex = 0;
+            this.ptbImagen.TabStop = false;
+            // 
+            // dgvImagen
+            // 
+            this.dgvImagen.AllowUserToAddRows = false;
+            this.dgvImagen.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvImagen.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvImagen.Location = new System.Drawing.Point(33, 36);
+            this.dgvImagen.Name = "dgvImagen";
+            this.dgvImagen.Size = new System.Drawing.Size(379, 420);
+            this.dgvImagen.TabIndex = 1;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(464, 247);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(216, 32);
+            this.txtNombre.TabIndex = 2;
+            this.txtNombre.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(461, 231);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nombre";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(498, 353);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(144, 51);
+            this.btnGuardar.TabIndex = 4;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnExaminar
+            // 
+            this.btnExaminar.Location = new System.Drawing.Point(515, 186);
+            this.btnExaminar.Name = "btnExaminar";
+            this.btnExaminar.Size = new System.Drawing.Size(97, 21);
+            this.btnExaminar.TabIndex = 5;
+            this.btnExaminar.Text = "Examinar";
+            this.btnExaminar.UseVisualStyleBackColor = true;
+            this.btnExaminar.Click += new System.EventHandler(this.btnExaminar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(778, 479);
+            this.Controls.Add(this.btnExaminar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.dgvImagen);
+            this.Controls.Add(this.ptbImagen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ptbImagen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvImagen)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ptbImagen;
+        private System.Windows.Forms.DataGridView dgvImagen;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnExaminar;
+    }
+}
+
---
+++ pjtImagenbaseDatos/clsConexiones.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Drawing;
+using System.IO;
+
+namespace pjtImagenbaseDatos
+{
+    class clsConexiones
+    {
+
+      public static  MySqlConnection cnx = new MySqlConnection("Server=127.0.0.1; database=BD_Imagen; Uid=root; pwd=1234;");
+
+
+        public static DataTable EjecutarQuery(string Q, Image img)
+        {
+            MemoryStream MS = new MemoryStream();
+            try
+            {
+                img.Save(MS, img.RawFormat);
+            }
+            catch
+            {
+
+
+            }
+
+
+            byte[] Imagenes = MS.GetBuffer();
+
+            DataTable dt = new DataTable();
+            MySqlCommand cmd = new MySqlCommand(Q, cnx);
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            cmd.Parameters.AddWithValue("@Imagen", Imagenes);
+
+            try
+            {
+                cnx.Open();
+                da.Fill(dt);
+                cnx.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally{ cnx.Close(); }
+
+            return dt;
+
+        }
+
+    }
+}
